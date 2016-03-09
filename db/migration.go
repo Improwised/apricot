4d9a6e66b25c463b128e5c41111b3b008bf54d7d
Malik <malik@improwised.com>
2016-03-09 18:08:05 +0530

    database configuration file created.

--- db/migration.go
+++ db/migration.go
@@ -1,15 +1,34 @@
 package main
 
-import "github.com/DavidHuie/gomigrate"
-import "database/sql"
-import _"github.com/lib/pq"
-import "fmt"
+import (
+  "github.com/DavidHuie/gomigrate"
+  "database/sql"
+  _"github.com/lib/pq"
+  "fmt"
+  "encoding/json"
+  "os"
+)
+
+type Configuration struct {
+  DbName string
+  UserName string
+}
 
 func main() {
-  dbinfo := fmt.Sprintf("user=mohit dbname=iims sslmode=disable")
+  file, _ := os.Open("../config/configuration.json")
+  decoder := json.NewDecoder(file)
+  configuration := Configuration{}
+  decoder.Decode(&configuration)
+
+  userName := configuration.UserName
+  dbName := configuration.DbName
+
+  dbinfo := fmt.Sprintf("user=%s dbname=%s sslmode=disable",
+    userName, dbName)
   db, err := sql.Open("postgres", dbinfo)
   checkErr(err)
   defer db.Close()
+
   migrator, err := gomigrate.NewMigrator(db, gomigrate.Postgres{}, "./migrations")
   checkErr(err)
   migrator.Migrate()
