16bed978964b778675641f9a685e94a270b513fb
Ashvin <ashvin@improwised.com>
2016-03-23 17:30:59 +0530

    generated unique hash key and make all the things hash dependent.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,72 @@
 function checkform(pform1){
-  var email = pform1.email.value;
-  var err={};
-  var validemail =/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+	var email = pform1.email.value;
+	var err={};
+	var validemail =/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-+]+\.[a-zA-Z]{2,4}$/;
 
-  //validate email
-  if(!(validemail.test(email))) {
-    err.message="Invalid email";
-    err.field=pform1.email;
-  }
-  if(err.message) {
-    document.getElementById('divError').innerHTML = err.message;
-    err.field.focus();
-    return false;
-  }
-  return true
+	//validate email
+	if(!(validemail.test(email))) {
+		err.message="Invalid email";
+		err.field=pform1.email;
+	}
+	if(err.message) {
+		document.getElementById('divError').innerHTML = err.message;
+		err.field.focus();
+		return false;
+	}
+	return true
 }
 
 //data retrive from html form
 function autoSave(data,id) {
-  var str = "";
-  str += data ;
-  retriveData(str,id);
+	var str = "";
+
+	str += data ;
+	url = window.location.search.substring(1);
+	var hash;
+	var myJson = {};
+
+	var hashes = url.slice(url.indexOf('?') + 1).split('&');
+
+	for (var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		myJson[hash[0]] = hash[1];
+	}
+	retriveData(str,id, myJson[hash[0]]);
 }
 
-function retriveData(str,id) {
-  if (str==""){
-    return;
-  }
-  if (window.XMLHttpRequest) {
-    xmlhttp=new XMLHttpRequest();
-  }
-  xmlhttp.open("GET","information?data="+str+ "&id="+ id,true);
-  xmlhttp.send();
+function retriveData(str,id, hash) {
+
+	if (str==""){
+		return;
+	}
+	if (window.XMLHttpRequest) {
+		xmlhttp=new XMLHttpRequest();
+	}
+	xmlhttp.open("GET","information?data="+str+ "&id="+ id + "&key=" + hash, true);
+	xmlhttp.send();
+}
+
+
+//giving success message
+function confirmMsg() {
+	var x = location.search;
+	if(x !== "") {
+	document.getElementById("messageSent").innerHTML = "Thank you for your interest.Check your email id for further process.";
+	}
+}
+
+//getting email and passing as hidden
+function emailHidden() {
+	url = window.location.search.substring(1);
+	var hash;
+	var myJson = {};
+	var hashes = url.slice(url.indexOf('?') + 1).split('&');
+
+	for (var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		myJson[hash[0]] = hash[1];
+ }
+
+ document.getElementById("email").value = myJson[hash[0]];
+
 }
