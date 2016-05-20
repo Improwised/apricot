cc0802f986746af162f4df92184b2bed154cab67
Mohit <mohit@improwised.com>
2016-05-20 17:51:44 +0530

    Pagination added.

--- assets/js/index.js
+++ assets/js/index.js
@@ -314,7 +314,7 @@ $(document).ready(function() {
 		//will set the theme of editor...
 		editor.setTheme("ace/theme/merbivore");
 		document.getElementById('editor').style.fontSize='16px';
-		document.getElementById('editor').style.letterSpacing = "0px";
+		// document.getElementById('editor').style.letterSpacing = "0px";
 	});
 });
 
@@ -381,3 +381,53 @@ function showDiv1() {
     document.getElementById('pad').style.display = "block";
 }
 
+//Pagination..............
+$('table.pagination').each(function() {
+    var currentPage = 0;
+    var numPerPage = 10;
+    var $table = $(this);
+    $table.bind('repaginate', function() {
+        $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+    });
+    $table.trigger('repaginate');
+    var numRows = $table.find('tbody tr').length;
+    var numPages = Math.ceil(numRows / numPerPage);
+    var $pager = $('<div  class="pager"></div>');
+    var $previous = $('<span class="previous"><<</spnan>');
+    var $next = $('<span class="next">>></spnan>');
+    for (var page = 0; page < numPages; page++) {
+        $('<span class="pagination"></span>').text(page + 1).bind('click', {
+            newPage: page
+        }, function(event) {
+            currentPage = event.data['newPage'];
+            $table.trigger('repaginate');
+            $(this).addClass('active').siblings().removeClass('active');
+        }).appendTo($pager).addClass('clickable');
+    }
+    $pager.insertAfter($table).find('span.pgination:first').addClass('active');
+    $previous.insertBefore('span.pagination:first');
+    $next.insertAfter('span.pagination:last');
+
+    $next.click(function (e) {
+        $previous.addClass('clickable');
+        $pager.find('.active').next('.pagination.clickable').click();
+    });
+    $previous.click(function (e) {
+        $next.addClass('clickable');
+        $pager.find('.active').prev('.pagination.clickable').click();
+    });
+    $table.on('repaginate', function () {
+        $next.addClass('clickable');
+        $previous.addClass('clickable');
+
+        setTimeout(function () {
+            var $active = $pager.find('.pagination.active');
+            if ($active.next('.pagination.clickable').length === 0) {
+                $next.removeClass('clickable');
+            } else if ($active.prev('.pagination.clickable').length === 0) {
+                $previous.removeClass('clickable');
+            }
+        });
+    });
+    $table.trigger('repaginate');
+});
