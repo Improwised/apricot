1fbeb01e4a331f46259676a24b8cc8fd8c69f6ce
Ashvin <ashvin@improwised.com>
2016-08-03 11:24:40 +0530

    migration updated ...

--- db/migration.go
+++ db/migration.go
@@ -4,38 +4,49 @@ import (
   "github.com/DavidHuie/gomigrate"
   "database/sql"
   _"github.com/lib/pq"
-  "fmt"
-  "encoding/json"
   "os"
+  "github.com/improwised/apricot/dbconfig"
 )
 
-type Configuration struct {
-  DbName string
-  UserName string
-}
-
 func main() {
-  file, _ := os.Open("../config/configuration.json")
-  decoder := json.NewDecoder(file)
-  configuration := Configuration{}
-  decoder.Decode(&configuration)
+  var connectionString string
+  var db *sql.DB
 
-  userName := configuration.UserName
-  dbName := configuration.DbName
-
-  dbinfo := fmt.Sprintf("user=%s dbname=%s sslmode=disable",
-    userName, dbName)
-  db, err := sql.Open("postgres", dbinfo)
-  checkErr(err)
-  defer db.Close()
-
-  migrator, err := gomigrate.NewMigrator(db, gomigrate.Postgres{}, "./migrations")
-  checkErr(err)
-  migrator.Migrate()
-}
-
-func checkErr(err error) {
-  if err != nil {
-    panic(err)
+  if(os.Getenv("GO_ENV2") == ""){
+    os.Setenv("GO_ENV2", "production")
   }
+
+  if(os.Getenv("GO_ENV2") == "production"){
+      connectionString = dbconfig.PostgresConnectionString("../dbconfig/test-files/production.json", "disable")
+      db, _ = sql.Open("postgres", connectionString)
+
+      migrator, err2 := gomigrate.NewMigrator(db, gomigrate.Postgres{}, "./migrations")
+      if err2 != nil {
+        panic(err2)
+      }
+      migrator.Migrate()
+  } else if((os.Getenv("GO_ENV2")) == "development") {
+      connectionString = dbconfig.PostgresConnectionString("./dbconfig/test-files/settings.json", "disable")
+      db, _ = sql.Open("postgres", connectionString)
+  } else {
+      connectionString = dbconfig.PostgresConnectionString("../dbconfig/test-files/testing.json", "disable")
+      db, _ = sql.Open("postgres", connectionString)
+
+      migrator, err2 := gomigrate.NewMigrator(db, gomigrate.Postgres{}, "../db/migrations")
+      if err2 != nil {
+        panic(err2)
+      }
+      migrator.Migrate()
+   }
+//************ uncomment following code to show connected user's info  **********
+  // settings := dbconfig.Settings(connectionString)
+  // connection := []string{
+  //   "host=", settings["host"], " ",
+  //   "password=", settings["password"], " ",
+  //   "user=", settings["username"], " ",
+  //   "dbname=", settings["database"], " ",
+  //   }
+  // fmt.Println(strings.Join(connection, ""))
+//**********************************************************************
+    defer db.Close()
 }
